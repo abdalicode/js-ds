f7fdb9faa0ccf38164456d8631017bec36407c7f
abdalicode <hakan.ab02@gmail.com>
2023-04-16 23:21:50 +0430

    singly linkedi list

--- src/singly-linked-list.ts
+++ src/singly-linked-list.ts
@@ -1,127 +1,127 @@
-// @ts-nocheck
 class ListNode {
-    constructor(public val, public next = null) {
-    }
+  constructor(public val, public next: ListNode | null = null) {}
 }
 
 class SinglyLinkedList {
-    constructor(public head = null, public tail = null, public length: number = 0) {
+  constructor(
+    public head: ListNode | null = null,
+    public tail: ListNode | null = null,
+    public length: number = 0
+  ) {}
+
+  push(val) {
+    const newNode = new ListNode(val);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail!.next = newNode;
+      this.tail = newNode;
     }
+    this.length += 1;
+    return this;
+  }
 
-    push(val) {
-        const newNode = new ListNode(val)
-
-        if (!this.head) {
-            this.head = newNode
-            this.tail = newNode
-        } else {
-            this.tail.next = newNode
-            this.tail = newNode
-        }
-        this.length += 1;
-        return this
+  pop() {
+    if (!this.head) return undefined;
+    let current = this.head;
+    let newTail = current;
+    while (current.next) {
+      newTail = current;
+      current = current.next;
     }
-
-    pop() {
-        if (!this.head) return undefined
-        let current = this.head
-        let newTail = current
-        while (current.next) {
-            newTail = current
-            current = current.next
-        }
-        this.tail = newTail
-        this.tail.next = null
-        this.length -= 1
-        if (this.length === 0) {
-            this.head = null
-            this.tail = null
-        }
-        return current
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length -= 1;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
     }
+    return current;
+  }
 
-    shift() {
-        if (!this.head) return undefined
-        const currentHead = this.head
-        this.head = currentHead.next
-        this.length -= 1
-        if (this.length === 0) {
-            this.tail = null
-        }
-        return currentHead
+  shift() {
+    if (!this.head) return undefined;
+    const currentHead = this.head;
+    this.head = currentHead.next;
+    this.length -= 1;
+    if (this.length === 0) {
+      this.tail = null;
     }
+    return currentHead;
+  }
 
-    unshift(val) {
-        let newNode = new ListNode(val)
-        if (!this.head) {
-            this.head = newNode
-            this.tail = newNode
-        } else {
-            newNode.next = this.head
-            this.head = newNode
-        }
-        this.length += 1
-        return this
+  unshift(val) {
+    let newNode = new ListNode(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
     }
+    this.length += 1;
+    return this;
+  }
 
-    get(index) {
-        if (index < 0 || index >= this.length) return null
-        let counter = 0
-        let current = this.head
-        while (counter !== index) {
-            current = current.next
-            counter += 1
-        }
-        return current
+  get(index) {
+    if (index < 0 || index >= this.length) return null;
+    let counter = 0;
+    let current = this.head;
+    while (counter !== index) {
+      current = current!.next;
+      counter += 1;
     }
+    return current;
+  }
 
-    set(index, val) {
-        let foundNode = this.get(index)
-        if (foundNode) {
-            foundNode.val = val
-            return true
-        }
-        return false
+  set(index, val) {
+    let foundNode = this.get(index);
+    if (foundNode) {
+      foundNode.val = val;
+      return true;
     }
+    return false;
+  }
 
-    insert(index, val) {
-        if (index < 0 || index > this.length) return false
-        if (index === this.length) return !!this.push(val)
-        if (index === 0) return !!this.unshift(val)
+  insert(index, val) {
+    if (index < 0 || index > this.length) return false;
+    if (index === this.length) return !!this.push(val);
+    if (index === 0) return !!this.unshift(val);
 
-        let newNode = new ListNode(val)
-        let prev = this.get(index - 1)
-        let temp = prev.next
-        prev.next = newNode
-        newNode.next = temp
-        this.length += 1
-        return true
+    let newNode = new ListNode(val);
+    let prev = this.get(index - 1);
+    let temp = prev!.next;
+    prev!.next = newNode;
+    newNode.next = temp;
+    this.length += 1;
+    return true;
+  }
+
+  remove(index) {
+    if (index < 0 || index >= this.length) return undefined;
+    if (index === 0) return this.shift();
+    if (index === this.length - 1) return this.pop();
+    const previousNode = this.get(index - 1);
+    const removed = previousNode!.next;
+    previousNode!.next = removed!.next;
+    this.length -= 1;
+    return removed;
+  }
+
+  reverse() {
+    let node = this.head;
+    this.head = this.tail;
+    this.tail = node;
+    let next;
+    let prev: ListNode | null = null;
+    for (let i = 0; i < this.length; i++) {
+      next = node!.next;
+      node!.next = prev;
+      prev = node;
+      node = next;
     }
-
-    remove(index) {
-        if (index < 0 || index >= this.length) return undefined
-        if (index === 0) return this.shift()
-        if (index === this.length - 1) return this.pop()
-        const previousNode = this.get(index - 1)
-        const removed = previousNode.next
-        previousNode.next = removed.next
-        this.length -= 1
-        return removed
-    }
-
-    reverse() {
-        let node = this.head
-        this.head = this.tail
-        this.tail = node
-        let next
-        let prev = null
-        for (let i = 0; i < this.length; i++) {
-            next = node.next
-            node.next = prev
-            prev = node
-            node = next
-        }
-        return this
-    }
+    return this;
+  }
 }
-
